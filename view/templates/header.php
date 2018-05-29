c2c5c0ea2c2cd63267ba83afa5ca47167972cef4
rens gouman <99030922@mydavinci.nl>
2018-05-29 14:34:24 +0200

    Add files via upload

---
+++ view/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Studenten app</title>	
+	<link rel="stylesheet" href="<?= URL ?>public/css/main.css">
+</head>
+<body>
+	<nav>
+	<ul>
+		<li><a href="<?= URL ?>home/index">Home</a></li>
+		<li><a href="<?= URL ?>student/index">Students</a></li>
+		<li><a href="<?= URL ?>song/index">Songs</a></li>
+		<li><a href="<?= URL ?>calendar/index">calendar</a></li>
+	</ul>
+	</nav>
